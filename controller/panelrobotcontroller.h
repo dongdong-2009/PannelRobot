43ab09764de24b4ec65b9d5b459672ee1c29b3f5
Gauss Cheng <GaussCheng@gmail.com>
2015-11-27 13:54:45 +0800

    1.修复IO监视问题

--- controller/panelrobotcontroller.h
+++ controller/panelrobotcontroller.h
@@ -383,8 +383,8 @@ public:
 #endif
     }
 
-    Q_INVOKABLE int iStatus(int boardID) { return ICRobotVirtualhost::IStatus(boardID);}
-    Q_INVOKABLE int oStatus(int boardID) { return ICRobotVirtualhost::OStatus(boardID);}
+    Q_INVOKABLE quint32 iStatus(int boardID) { return ICRobotVirtualhost::IStatus(boardID);}
+    Q_INVOKABLE quint32 oStatus(int boardID) { return ICRobotVirtualhost::OStatus(boardID);}
 
 
 signals:

459a6d351def6be8c4aa2ed99b1356d925ae0c4b
Gauss Cheng <GaussCheng@gmail.com>
2015-12-23 10:23:06 +0800

    1.修改计数器传输

--- controller/panelrobotcontroller.cpp
+++ controller/panelrobotcontroller.cpp
@@ -896,7 +896,7 @@ void PanelRobotController::OnHostUpdateFinished(QString)
 
 bool PanelRobotController::saveCounterDef(quint32 id, const QString &name, quint32 current, quint32 target)
 {
-    ICRobotVirtualhost::SendMoldCounterDef(host_, QVector<quint32>()<<id<<current<<target);
+    ICRobotVirtualhost::SendMoldCounterDef(host_, QVector<quint32>()<<id<<target<<current);
     return ICRobotMold::CurrentMold()->CreateCounter(id, name, current, target);
 }
 

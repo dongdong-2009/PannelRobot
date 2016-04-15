f36625ce0280a1702808156e6bed81d9133a77ae
Gauss Cheng <GaussCheng@gmail.com>
2016-04-15 18:47:06 +0800

    1.gcode

--- qml/App_HAMOUI/teach/RecordManagementPage.js
+++ qml/App_HAMOUI/teach/RecordManagementPage.js
@@ -6,6 +6,15 @@ var HCGCodeInterpreter = {
 
     createNew:function(options){
         var interpreter = GCodeInterpreter.createNew(options);
+        var cmdGrouMap = {
+            "G0":1,"G1":1,"G2":1,"G3":1,
+            "G17":2,"G18":2,"G19":2
+
+        };
+        var currentPos = {
+            "X":0,"Y":0,"Z":0
+        }
+
         interpreter.G92 = function(args){
             console.log("G92", args);
         }

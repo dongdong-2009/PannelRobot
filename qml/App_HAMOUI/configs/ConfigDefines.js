0f3f2d185c4f7612abd8764881340a9a4b0f8314
Gauss Cheng <GaussCheng@gmail.com>
2016-08-12 09:24:28 +0800

    1.翻译

--- qml/App_HAMOUI/configs/ConfigDefines.js
+++ qml/App_HAMOUI/configs/ConfigDefines.js
@@ -16,6 +16,10 @@ var configRanges = {
     "m_rw_0_16_0_1":[2, 200],
     "m_rw_0_16_0_2":["m_rw_0_16_0_0", "m_rw_0_16_0_1"],
     "m_rw_0_16_0_16":[0, MAX_UINT_16],
+    "m_rw_0_32_3_800":[MIN_INT_32, MAX_INT_32],
+    "m_rw_0_32_3_801":[MIN_INT_32, MAX_INT_32],
+    "m_rw_0_32_3_802":[MIN_INT_32, MAX_INT_32],
+
     "s_rw_0_16_1_294":[0, 100], //manual speed
 //    "s_rw_0_16_1_265":[0, 100], //auto speed
     "s_rw_0_16_0_102":[MIN_INT_16, MAX_INT_16],

97ffaed2335b77d20d73e998d350efd30fc825f2
Gauss Cheng <GaussCheng@gmail.com>
2016-08-17 14:48:36 +0800

    1.添加单轴加减速度

--- qml/App_HAMOUI/translations/HAMOUI_zh_CN.ts
+++ qml/App_HAMOUI/translations/HAMOUI_zh_CN.ts
@@ -1071,6 +1071,22 @@
         <source>Motor 4</source>
         <translation>Motor4</translation>
     </message>
+    <message>
+        <source>SACC1</source>
+        <translation>S加速1</translation>
+    </message>
+    <message>
+        <source>SACC2</source>
+        <translation>S加速2</translation>
+    </message>
+    <message>
+        <source>SDCC1</source>
+        <translation>S减速1</translation>
+    </message>
+    <message>
+        <source>SDCC2</source>
+        <translation>S减速2</translation>
+    </message>
 </context>
 <context>
     <name>AxisDefine</name>

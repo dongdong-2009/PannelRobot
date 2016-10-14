ba1e82de9c5ffe5a416c132f1f1eced0391efff6
Gauss Cheng <GaussCheng@gmail.com>
2016-10-14 15:42:40 +0800

    1.add gitignore

---
+++ PanelRobot_en_US.ts
@@ -0,0 +1,145 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>ICSplashScreen</name>
+    <message>
+        <location filename="icsplashscreen.cpp" line="14"/>
+        <source>SW Version:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="icsplashscreen.cpp" line="16"/>
+        <source>HW Version:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="icsplashscreen.cpp" line="18"/>
+        <source>Client:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="icsplashscreen.cpp" line="26"/>
+        <source>Shiment Date:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ICUISelector</name>
+    <message>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icuiselector.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icuiselector.ui" line="45"/>
+        <source>Select UI Main:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icuiselector.ui" line="52"/>
+        <source>选择UI主文件:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icuiselector.ui" line="71"/>
+        <source>Browse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icuiselector.ui" line="78"/>
+        <source>浏览</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ICVirtualKeyboard</name>
+    <message>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.ui" line="493"/>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.cpp" line="109"/>
+        <source>Ent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.ui" line="435"/>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.cpp" line="128"/>
+        <source>CE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.ui" line="1081"/>
+        <source>ENT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.ui" line="1032"/>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.cpp" line="370"/>
+        <source>EN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.ui" line="1000"/>
+        <source>Space</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.ui" line="127"/>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.ui" line="1308"/>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.cpp" line="135"/>
+        <source>BS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.ui" line="352"/>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.ui" line="1048"/>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.cpp" line="120"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.cpp" line="307"/>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.cpp" line="375"/>
+        <source>CH</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vendor/IndustrialSystemFramework/ICUtility/icvirtualkeyboard.cpp" line="441"/>
+        <source>Min:%1
+Max:%2
+Prec:%3</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PanelRobotController</name>
+    <message>
+        <location filename="controller/panelrobotcontroller.cpp" line="239"/>
+        <location filename="controller/panelrobotcontroller.cpp" line="244"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="controller/panelrobotcontroller.cpp" line="239"/>
+        <source>Database is error!!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="controller/panelrobotcontroller.cpp" line="244"/>
+        <source>Open Database fail!!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QJson::ParserRunnable</name>
+    <message>
+        <location filename="vendor/IndustrialSystemFramework/QJson/parserrunnable.cpp" line="64"/>
+        <source>An error occurred while parsing json: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

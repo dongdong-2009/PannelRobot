c4ca7a2d7ad9b0409cae76a1a7b2256ad77601dc
luoyayun <luoyayun@hc-system.com>
2017-01-10 10:11:03 +0800

    翻译

--- qml/App_HAMOUI/translations/HAMOUI_zh_CN.ts
+++ qml/App_HAMOUI/translations/HAMOUI_zh_CN.ts
@@ -6314,7 +6314,7 @@ please chose!</source>
     </message>
     <message>
         <source>confirmBtn</source>
-        <translation type="unfinished">坐标系换算</translation>
+        <translation>确定修改</translation>
     </message>
 </context>
 <context>
